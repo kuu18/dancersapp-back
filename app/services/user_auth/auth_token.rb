19baf961cd25e38137c9103e679e40c2abc515e9
github user name <kdai18.kuu@gmail.com>
2021-02-16 09:03:23 +0900

    add_authenticator.rb

--- app/services/user_auth/auth_token.rb
+++ app/services/user_auth/auth_token.rb
@@ -18,6 +18,11 @@ module UserAuth
       end
     end
 
+    # subjectからユーザーを検索する
+    def entity_for_user
+      User.find @payload["sub"]
+    end
+
     private
 
       # エンコードキー(config/initializers/user_auth.rb)
