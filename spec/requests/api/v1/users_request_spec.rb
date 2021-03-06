997c63298560cf732ac575da530c0f104555598c
github user name <kdai18.kuu@gmail.com>
2021-03-06 23:08:18 +0900

    add_signup_function

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,14 +2,92 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Users', type: :request do
   describe 'UserAPI' do
-    let(:user) { create(:user) }
+    describe 'POST /api/v1/users' do
+      context 'when valid request' do
+        let(:user_params) do
+          attributes_for(:user, name: 'Test',
+                                email: 'test@example.com',
+                                password: 'password')
+        end
 
-    before do
-      logged_in(user)
+        it 'adds a user' do
+          expect do
+            post '/api/v1/users', params: { user: user_params }
+          end.to change(User, :count).by(1)
+        end
+
+        it 'response 200' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response.status).to eq 200
+        end
+
+        it 'response include type info' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response_body['type']).to eq 'info'
+        end
+
+        it 'response include correct msg' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response_body['msg']).to eq '認証メールを送信しました。'
+        end
+
+        it 'response include errors nill' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response_body['errors']).to eq nil
+        end
+      end
+
+      context 'when invalid request' do
+        let(:user_params) do
+          attributes_for(:user, name: '',
+                                email: 'user@invalid',
+                                password: '')
+        end
+
+        it 'does not add a user' do
+          expect do
+            post '/api/v1/users', params: { user: user_params }
+          end.to change(User, :count).by(0)
+        end
+
+        it 'response 200' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response.status).to eq 200
+        end
+
+        it 'response include type error' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response_body['type']).to eq 'error'
+        end
+
+        it 'response include msg nill' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response_body['msg']).to eq nil
+        end
+
+        it 'response include errors email' do
+          post '/api/v1/users', params: { user: user_params }
+          expect(response_body['errors']).to include '名前を入力してください', 'メールアドレスは不正な値です', 'パスワードを入力してください'
+        end
+      end
+
+      context 'when email already taken' do
+        before do
+          FactoryBot.create(:user, email: 'test@example.com', activated: true)
+        end
+
+        it 'response include errors already taken' do
+          post '/api/v1/users', params: { user: attributes_for(:user, email: 'test@example.com') }
+          expect(response_body['errors']).to eq 'メールアドレスはすでに存在します'
+        end
+      end
     end
 
-    describe '/api/v1/users/current_user' do
+    describe 'GET /api/v1/users/current_user' do
+      let(:user) { create(:user) }
+
       before do
+        logged_in(user)
         get '/api/v1/users/current_user'
       end
 
