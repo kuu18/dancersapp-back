1d87df60c5bd5a840164e9ab10a23eddc9e0d4f4
github user name <kdai18.kuu@gmail.com>
2021-03-15 14:01:08 +0900

    add_users_profile_settings

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create] do
         get :current_user, action: :show, on: :collection
+        patch :update_profile, action: :update, on: :collection
+        patch :chenge_email, action: :change_email, on: :collection
+        patch :chenge_password, action: :change_password, on: :collection
       end
+      delete '/users', to: 'users#destroy'
       resources :login, only: [:create]
-      delete 'logout', to: 'login#destroy'
+      delete '/logout', to: 'login#destroy'
       resources :account_activations, only: [:index]
       resources :password_resets,     only: %i[create index]
       patch '/password_resets', to: 'password_resets#update'
